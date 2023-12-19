257988d0be889ff56be48330d3adbab02e0e0975
aa bb <aaaa@b.com>
2023-12-20 05:59:45 +0900

    add gogs.

--- dockerfile/nginx/startnginx.sh
+++ dockerfile/nginx/startnginx.sh
@@ -5,8 +5,13 @@ envsubst \
     '$PATH_SERVERCRT $PATH_SERVERKEY $PATH_CLIENTCRT $URL_WIKIJS' < \
     /etc/nginx/conf.d/wikijs.template > \
     /etc/nginx/conf.d/wikijs.conf
+envsubst \
+    '$PATH_SERVERCRT $PATH_SERVERKEY $PATH_CLIENTCRT $URL_GOGS' < \
+    /etc/nginx/conf.d/gogs.template > \
+    /etc/nginx/conf.d/gogs.conf
 
 # Delete template config file.
 rm -f /etc/nginx/conf.d/wikijs.template
+rm -f /etc/nginx/conf.d/gogs.template
 
 nginx -g "daemon off;"
